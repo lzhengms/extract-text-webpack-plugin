2c6365935e15e5f0357aff7dfe10149b07bf83ba
lzhengms <lzhengms@gmail.com>
2017-01-05 19:01:57 +0800

    add option for order

--- index.js
+++ index.js
@@ -289,11 +289,13 @@ ExtractTextPlugin.prototype.apply = function(compiler) {
 			extractedChunks.forEach(function(extractedChunk) {
 				if(extractedChunk.modules.length) {
 					extractedChunk.modules.sort(function(a, b) {
+					if(!options.ignoreOrder){
 						if(isInvalidOrder(a, b)) {
 							compilation.errors.push(new OrderUndefinedError(a.getOriginalModule()));
 							compilation.errors.push(new OrderUndefinedError(b.getOriginalModule()));
 						}
 						return getOrder(a, b);
+					}
 					});
 					var chunk = extractedChunk.originalChunk;
 					var source = this.renderExtractedChunk(extractedChunk);
